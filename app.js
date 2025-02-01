9a0643e234997b2fe4d5c881056490920320803e
Gato-01 <stevanvalbuena@gmail.com>
2025-02-01 16:20:52 -0500

    V2

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];  
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");  
+    let nombre = input.value.trim(); 
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+     }
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya está en la lista.");
+        return;
+    }
+
+    amigos.push(nombre);  
+    actualizarLista();  
+    input.value = "";  
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach((amigo, index) => {
+        let item = document.createElement("li");
+        item.textContent = `${index + 1}. ${amigo}`;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Debes agregar al menos 2 amigos para hacer el sorteo.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let ganador = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>¡El amigo secreto es: <strong>${ganador}</strong>! 🎁</li>`;
+}
